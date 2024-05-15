68be64b89fdfed32b306e8e61875d82e809e693f
adrianmehaj <adrianmehaj123@gmail.com>
2024-05-15 20:48:25 +0200

    Madhësia dhe pozicionimi i dritares

--- Base64.py
+++ Base64.py
@@ -58,3 +58,12 @@ def decode_base64():
 # GUI setup
 root = tk.Tk()
 root.title("Base64 Encoder/Decoder")
+
+# Vendos madhësinë dhe pozicionin e dritares
+window_width = 400
+window_height = 200
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x_coordinate = (screen_width / 2) - (window_width / 2)
+y_coordinate = (screen_height / 2) - (window_height / 2)
+root.geometry('%dx%d+%d+%d' % (window_width, window_height, x_coordinate, y_coordinate))
