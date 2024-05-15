87dd7e055571903973570b1022170d9a8c77d7e8
Afrim Ymeri <afrimymeri0@gmail.com>
2024-05-15 15:39:38 +0200

    Initial commit

--- Base64.py
+++ Base64.py
@@ -1,1 +1,10 @@
 import tkinter as tk
+
+def custom_base64_encode(message):
+    base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+    encoded_chars = []
+
+    #Konvertimi i karaktereve te mesazhit ne ASCII
+    ascii_values = [ord(char) for char in message]
+
+    binary_strings = ['{:08b}'.format(ascii_val) for ascii_val in ascii_values]
